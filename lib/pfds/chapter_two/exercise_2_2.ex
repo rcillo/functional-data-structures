03b95433059a12802430be9c541a913f7e14429b
Ricardo de Cillo <ricardo.cillo@plataformatec.com.br>
2014-03-23 23:19:29 -0300

    binary search tree add operation without unnecessary copying and with at most d + 1 comparisons

--- lib/pfds/chapter_two/exercise_2_2.ex
+++ lib/pfds/chapter_two/exercise_2_2.ex
@@ -14,4 +14,28 @@ defmodule PFDS.FasterBinaryTree do
   def member(x, nil, c) do
     x == c
   end
+
+  def add(x, t) do
+    try do
+      do_add(x, t, nil)
+    rescue
+      _ -> t
+    end
+  end
+
+  def do_add(x, nil, c) do
+    if x == c do
+      raise "element already present"
+    else
+      {nil, x, nil}
+    end
+  end
+
+  def do_add(x, t = {l, y, r}, c) do
+    if x < y do
+      {do_add(x, l, c), y, r}
+    else
+      {l, y, do_add(x, r, y)}
+    end
+  end
 end
